f9a648d56d8aa88bf8a507b190beb7212a1c6213
James Sheehan <james_sheehan@neimanmarcus.com>
2020-05-19 15:22:41 -0500

    basic setup

---
+++ server/index.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const session = require("express-session");
+const passport = require("passport");
+const { json } = require("body-parser");
+const massive = require("massive");
+const cors = require("cors");
+const port = 3001
+
+//setup
+app.use(json());
+app.use(cors());
+
+//session
+// app.use(
+//   session({
+//     resave: false,
+//     saveUninitialized: true,
+//     secret: process.env.SESSION_SECRET,
+//     cookie: {
+//       maxAge: 1000 * 600 * 60 * 24 * 7
+//     }
+//   })
+// )
+
+//auth
+app.use(passport.initialize());
+app.use(passport.session());
+
+//db
+
+//server
+app.listen(port, () => console.log(`Listening on ${port}`));
