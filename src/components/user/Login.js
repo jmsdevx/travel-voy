c109fb20c0eaa8c3527325f73224c0e131059c6b
James Sheehan <james_sheehan@neimanmarcus.com>
2020-06-05 11:01:44 -0500

    style login

--- src/components/user/Login.js
+++ src/components/user/Login.js
@@ -1,13 +1,18 @@
 import React from "react";
 import Header from "../layout/header/Header";
+import Button from "../general/Button";
+import { Link } from "react-router-dom";
 
 function Login() {
   return (
     <div className="login">
       <Header />
-      <h1>Login</h1>
-      <input type="text" />
-      <input type="text" />
+      <h1 className="heading">Login</h1>
+      <input type="text" className="input" placeholder="username" />
+      <input type="password" className="input" placeholder="password" />
+      <Link to="/profile">
+        <Button title="Login" onClick={null} className="log-button" />
+      </Link>
     </div>
   );
 }
--- src/components/user/Profile.js
+++ src/components/user/Profile.js
@@ -1,7 +1,15 @@
 import React from "react";
+import Header from "../layout/header/Header";
 
 function Profile() {
-  return <div>Profile</div>;
+  return (
+    <>
+      <Header />
+      <div className="profile">
+        <h1>**Profile**</h1>
+      </div>
+    </>
+  );
 }
 
 export default Profile;
